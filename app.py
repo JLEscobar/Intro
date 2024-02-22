9f04f51093e73c26e7d30981d1366b238f359209
JLEscobar <160857638+JLEscobar@users.noreply.github.com>
2024-02-22 07:20:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 from PIL impor Image
-title("mi primera app")
+st.title("mi primera app")
+
+st.header("En este espacio comiezo a desarrollar mis aplicaciones para interfaces multimodales")
+st.write("facilmente pudo realizar banked y fronted")
