22b68d4cdad001ef953045f713880aa8b2a17077
JLEscobar <160857638+JLEscobar@users.noreply.github.com>
2024-02-22 07:27:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.title("mi primera app")
 st.header("En este espacio comiezo a desarrollar mis aplicaciones para interfaces multimodales")
 st.write("facilmente pudo realizar banked y fronted")
 image = Image.open("Avatar_balial.png")
+
+st.image(image, caption'Avatar belial')
